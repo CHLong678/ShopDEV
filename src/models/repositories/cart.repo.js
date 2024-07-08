e05d8d1cad9308fb5b0275d859ae3962680462f4
CHLong678 <caolong2503@gmail.com>
2024-07-08 22:16:42 +0700

    check code

--- src/models/repositories/cart.repo.js
+++ src/models/repositories/cart.repo.js
@@ -93,65 +93,71 @@ const deleteCart = async ({ userId, productId }) => {
   return await cartModel.updateOne(query, updateSet);
 };
 
-// const updateUserCartQuantity = async ({ userId, product }) => {
-//   const { productId, quantity } = product;
+module.exports = {
+  createUserCart,
+  updateUserCartQuantity,
+  deleteCart,
+};
 
-//   const query = {
-//       cart_userId: userId,
-//       "cart_products.productId": productId,
-//       cart_state: "active",
-//     },
-//     updateSet = {
-//       $inc: {
-//         "cart_products.$.quantity": quantity,
-//       },
-//     },
-//     options = {
-//       upsert: true,
-//       new: true,
-//     };
+/*
+"use strict";
+const cartModel = require("../cart.model");
 
-//   return await cartModel.findOneAndUpdate(query, updateSet, options);
-// };
+const createUserCart = async ({ userId, product }) => {
+  const query = {
+      cart_userId: userId,
+      cart_state: "active",
+    },
+    updateOrInsert = {
+      $addToSet: {
+        cart_products: product,
+      },
+    },
+    options = {
+      upsert: true,
+      new: true,
+    };
+  return await cartModel.findOneAndUpdate(query, updateOrInsert, options);
+};
+
+const updateUserCartQuantity = async ({ userId, product }) => {
+  const { productId, quantity } = product;
+
+  const query = {
+      cart_userId: userId,
+      "cart_products.productId": productId,
+      cart_state: "active",
+    },
+    updateSet = {
+      $inc: {
+        "cart_products.$.quantity": quantity,
+      },
+    },
+    options = {
+      upsert: true,
+      new: true,
+    };
+
+  return await cartModel.findOneAndUpdate(query, updateSet, options);
+};
+
+const deleteCart = async ({ userId, productId }) => {
+  const query = {
+      cart_userId: userId,
+      cart_state: "active",
+    },
+    updateSet = {
+      $pull: {
+        cart_products: { productId },
+      },
+    };
+
+  return await cartModel.updateOne(query, updateSet);
+};
 
 module.exports = {
   createUserCart,
   updateUserCartQuantity,
   deleteCart,
 };
-// "use strict";
-// const cartModel = require("../cart.model");
-// const createUserCart = async ({ userId, product }) => {
-//   const query = {
-//       cart_userId: userId,
-//       cart_state: "active",
-//     },
-//     updateOrInsert = {
-//       $addToSet: {
-//         cart_products: product,
-//       },
-//     },
-//     options = {
-//       upsert: true,
-//       new: true,
-//     };
-//   return await cartModel.findOneAndUpdate(query, updateOrInsert, options);
-// };
-// const deleteCart = async ({ userId, productId }) => {
-//   const query = {
-//       cart_userId: userId,
-//       cart_state: "active",
-//     },
-//     updateSet = {
-//       $pull: {
-//         cart_products: { productId },
-//       },
-//     };
-
-//   return await cartModel.updateOne(query, updateSet);
-// };
-// module.exports = {
-//   createUserCart,
-//   updateUserCartQuantity,
-//   deleteCart,
-// };
+*/
